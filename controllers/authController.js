133be8fb870d2e979f5078408f31bd273536b3bb
leehaim8 <leehaim811@gmail.com>
2024-12-30 18:40:08 +0200

    Create the login with the token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 
@@ -28,6 +29,29 @@ const authController = {
 
             await newUser.save();
             res.status(200).json({ message: "User registered successfully", user: newUser });
+        } catch (error) {
+            res.status(500).json({ message: "Server error", error });
+        }
+    },
+    async login(req, res) {
+        const { username, password } = req.body;
+        try {
+            const user = await User.findOne({ username: username });
+            if (!user) {
+                return res.status(404).json({ message: "User not found" });
+            }
+
+            if (password !== user.password) {
+                return res.status(401).json({ message: "Invalid password" });
+            }
+
+            const payload = { username: user.username, role: user.role };
+            const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '10m' })
+            res.status(200).json({ 
+                message: "User logged in successfully", 
+                token, 
+                user: { username: user.username, role: user.role } 
+            });
         } catch (error) {
             res.status(500).json({ message: "Server error", error });
         }
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -4,5 +4,6 @@ const { authController } = require('../controllers/authController');
 const authRouter = new Router();
 
 authRouter.post('/register', authController.register);
+authRouter.post('/login', authController.login);
 
 module.exports = { authRouter };
