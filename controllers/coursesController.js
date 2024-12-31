8d9308f2ddcc157efc0fb96786f4769f8cd90d48
leehaim8 <leehaim811@gmail.com>
2024-12-31 12:42:19 +0200

    Creating a get of all courses for a faculty user

--- controllers/coursesController.js
+++ controllers/coursesController.js
@@ -1,7 +1,14 @@
 const Course = require("../models/courseModel");
 
 const coursesController = {
-
+    async getUsers(req, res) {
+        try {
+            const courses = await Course.find();
+            res.status(200).json(courses);
+        } catch (error) {
+            res.status(500).json({ message: "Server error", error });
+        }
+    }
 };
 
 module.exports = { coursesController };
--- routers/coursesRouter.js
+++ routers/coursesRouter.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const { coursesController } = require('../controllers/coursesController');
-
+const { authController } = require('../controllers/authController');
 const coursesRouter = new Router();
+
+coursesRouter.get('/', authController.authToken, authController.authRole("staff"), coursesController.getUsers);
 
 module.exports = { coursesRouter };
