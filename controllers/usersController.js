55912e7594daed70f6231b2f1f4e2d7229b2e734
leehaim8 <leehaim811@gmail.com>
2024-12-31 18:29:31 +0200

    Creating a function to get all courses of a student user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,19 @@
 const User = require("../models/userModel");
+const Course = require('../models/courseModel');
 
 const usersController = {
-
+    async getCoursesOfUser(req, res) {
+        const { userID } = req.params;
+        try {
+            const user = await User.findById(userID);
+            if (!user) {
+                return res.status(404).json({ message: "User not found" });
+            }
+            res.status(200).json(user.courses);
+        } catch (error) {
+            res.status(500).json({ message: "Server error", error });
+        }
+    }
 };
 
 module.exports = { usersController };
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,6 +1,9 @@
 const { Router } = require('express');
 const { usersController } = require('../controllers/usersController');
+const { authController } = require('../controllers/authController');
 
 const usersRouter = new Router();
+
+usersRouter.get('/:userID', authController.authToken, authController.authRole("student"), usersController.getCoursesOfUser);
 
 module.exports = { usersRouter };
